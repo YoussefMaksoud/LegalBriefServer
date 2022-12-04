9ea05aa7489f5d035ec4693acf107089d087910a
youssef <youssef.abdelmak@gmail.com>
2022-12-04 12:01:11 -0700

    backend

---
+++ server/index.js
@@ -0,0 +1,65 @@
+//server index
+
+const { json } = require("express");
+const express = require("express");
+var mysql = require('mysql');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "legalbrief"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected to LegalBrief Database");
+});
+
+// con.on("connect", err => {
+//     if(err){
+//         console.error(err.message);
+//     }else{
+//         console.log("reading table");
+//         con.query("SELECT * FROM legaltree", (err, rows, fields) => {
+//             if(err){
+//                 console.log(err);
+//             }else{
+//                 console.log(rows);
+//             }
+//         });
+//     }
+// })
+
+app.get("/optionsInitial", (req, res) => {
+    console.log("reading table");
+    con.query("SELECT * FROM legaltree WHERE parent_name = 'none'", (err, rows, fields) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json({message: rows});
+        }
+    });
+});
+
+app.get("/nextOptionSet/:id", (req, res) => {
+    var parent = req.params.id;
+    var requestString = "SELECT * FROM legaltree WHERE parent_name = '" + parent + "'";
+    console.log(requestString);
+    con.query(requestString, (err, rows, fields) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json({message: rows});
+        }
+    });
+});
+
+app.listen(PORT, () => {
+
+    console.log(`Server listening on ${PORT}`);
+});
